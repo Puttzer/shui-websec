fe697fe0959fead0bfa7ebe52c92883c95e2fb5f
Patrick Carragher <carragher@live.se>
2020-11-11 16:27:47 +0100

    Cleaned up server.js

--- backend/server.js
+++ backend/server.js
@@ -14,72 +14,12 @@ const streams = new Datastore({
 	filename: './streams.db',
 	autoload: true
 })
-// const streamsDB = require('./data/flowItems.json')
-
-// streamsDB.streams.forEach(stream => {
-// 	streams.insert({
-// 		date: stream.date,
-// 		tags: stream.tags,
-// 		content: cryptr.encrypt(stream.content)
-// 	})
-// })
-
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// get all users
-app.get('/users', (request, response) => {
-	db.find({}, (error, users) => {
-		response.send(users)
-	})
-})
-//get a single user
-app.get('/users/:id', (request, response) => {
-	// db.findOne({ _id: request.params.id }, (error, user) => {
-	// 	if (user) { response.send(user) } else {
-	// 		response.send('you done fucked up a-aron')
-	// 	}
-	// })
 
-})
-//UPATEDE
-app.put('/users/:id', (request, response) => {
-	// const username = request.body.username
-	// const password = request.body.password
-
-	// db.update({ _id: request.params.id }, { username, password }, () => {
-	// 	response.send(`the username: ${username} has been updated`)
-	// })
-})
-//DELELELELELTE
-app.delete('/users/:id', (request, response) => {
-	// const username = request.body.username
-	// const password = request.body.password
-
-	// db.remove({ _id: request.params.id }, {}, () => {
-	// 	response.send(`the username: ${username} has been removed`)
-	// })
-})
-
-//create a user
-app.post('/users', (request, response) => {
-	// const username = request.body.username
-	// const password = request.body.password
-
-	// db.find({ username }, (error, users) => {
-	// 	if (users.length === 0) {
-	// 		db.insert({ username, password }, function (err, newDoc) {
-	// 			response.send('User inserted into database')
-	// 		});
-	// 	} else {
-	// 		response.send(`the username: ${username} is already in use`)
-	// 	}
-	// })
-	// console.log(request.body)
-
-})
 
 //FRONTEND
 app.post('/api/register', async (request, response) => {
